d2c68b8e4daae7a9ad98b342eb5dde4c63b93c5d
gorilla330 <m.172cm60kg.coin@gmail.com>
2025-03-05 14:41:55 +0900

    Add PostCSS plugins and update configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   plugins: [
+    require('postcss-flexbugs-fixes'),
+    require('postcss-preset-env')({ stage: 3 }),
     require('@tailwindcss/postcss7-compat'),
     require('autoprefixer'),
   ],
